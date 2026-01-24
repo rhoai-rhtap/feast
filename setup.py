2c263f40c488a7a882f69e3ad5274dd5e0ce60da
red-hat-konflux <126015336+red-hat-konflux[bot]@users.noreply.github.com>
2026-01-24 10:28:37 +0000

    chore(deps): update dependency pre-commit to <3.8.1
    Signed-off-by: red-hat-konflux <126015336+red-hat-konflux[bot]@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -221,7 +221,7 @@ CI_REQUIRED = (
         "Sphinx>4.0.0,<7",
         "testcontainers==4.9.0",
         "python-keycloak==4.2.2",
-        "pre-commit<3.3.2",
+        "pre-commit<3.8.1",
         "assertpy==1.1",
         "pip-tools",
         "pybindgen",
