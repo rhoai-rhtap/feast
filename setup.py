db381ae7562e34df50dc109e808c8ed9a39845c9
red-hat-konflux <126015336+red-hat-konflux[bot]@users.noreply.github.com>
2026-01-24 05:15:07 +0000

    chore(deps): update dependency pybindgen to v0.22.1
    Signed-off-by: red-hat-konflux <126015336+red-hat-konflux[bot]@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -386,7 +386,7 @@ setup(
     entry_points={"console_scripts": ["feast=feast.cli.cli:cli"]},
     use_scm_version=use_scm_version,
     setup_requires=[
-        "pybindgen==0.22.0",  # TODO do we need this?
+        "pybindgen==0.22.1",  # TODO do we need this?
         "setuptools_scm>=6.2",  # TODO do we need this?
     ],
 )
